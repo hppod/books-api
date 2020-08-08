fc4bd38762231858b427dc24bb89be07559f4a3c
hppod <hppodgornik@gmail.com>
2020-08-08 19:03:58 -0300

    Criando o Schema Livro

---
+++ src/app/models/livro.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require("mongoose")
+
+const LivroSchema = new Schema({
+    nome: {
+        type: String,
+        required: true,
+        maxlength: 500,
+        trim: true
+    },
+    paginas: {
+        type: Number,
+        required: false
+    },
+    autor: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    editora: {
+        type: String,
+        required: true,
+        trim: true
+    }
+},
+    {
+        versionKey: false,
+        timestamps: true
+    }
+)
+module.exports = modeel('livroschema', LivroSchema)
