fa9fdaa027f881fc0378f8153485347699a3903d
hppod <hppodgornik@gmail.com>
2020-08-08 18:50:52 -0300

    Configuração inicial do servidor express

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-console.log("Olá, mundo")
-console.log("Tudo bem?")
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const PORT = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.text())
+app.use(bodyParser.json())
+app.use(bodyParser.json({ type: 'application/json' }))
+
+app.use(cors())
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.send({ message: `API books-api ouvindo na porta ${PORT}` })
+})
+
+app.listen(PORT, () => console.log(`API ouvindo na porta ${PORT}`))
