1cb87409693e0d0c1cb88c6e3fdf5430b11b8119
hppod <hppodgornik@gmail.com>
2020-08-08 19:17:04 -0300

    Criando novos livros no banco de dados

--- src/app/controllers/livro.js
+++ src/app/controllers/livro.js
@@ -11,5 +11,15 @@ class Livro {
             }
         })
     }
+
+    post(req, res) {
+        livroschema.create(req.body, (err) => {
+            if (err) {
+                res.status(500).json({ message: "Houve um erro ao processar sua requisição", error: err })
+            } else {
+                res.status(201).json({ message: "Livro criado com sucesso" })
+            }
+        })
+    }
 }
 module.exports = new Livro()
--- src/app/routes/livro.js
+++ src/app/routes/livro.js
@@ -3,5 +3,6 @@ const route = express.Router()
 const Livro = require("./../controllers/livro")
 
 route.get("/listar-todos", Livro.get)
+route.post("/criar", Livro.post)
 
 module.exports = route
