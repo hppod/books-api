35c4f7ff568089a7266f82f206c997fd24000420
hppod <hppodgornik@gmail.com>
2020-08-08 19:26:02 -0300

    Apagando um livro no banco de dados

--- src/app/controllers/livro.js
+++ src/app/controllers/livro.js
@@ -41,5 +41,15 @@ class Livro {
             }
         })
     }
+
+    delete(req, res) {
+        livroschema.deleteOne({ nome: req.params.nome }, (err) => {
+            if (err) {
+                res.status(500).json({ message: "Houve um erro ao processar sua requisição", error: err })
+            } else {
+                res.status(200).json({ message: "Livro apagado com sucesso" })
+            }
+        })
+    }
 }
 module.exports = new Livro()
--- src/app/routes/livro.js
+++ src/app/routes/livro.js
@@ -6,5 +6,6 @@ route.get("/listar-todos", Livro.get)
 route.get("/listar-um/:nome", Livro.getByName)
 route.post("/criar", Livro.post)
 route.put("/atualizar/:nome", Livro.put)
+route.delete("/apagar/:nome", Livro.delete)
 
 module.exports = route
