d53ace0cca6b6c2d600e9dd4f4c1dfb579739cbd
hppod <hppodgornik@gmail.com>
2020-08-22 20:04:52 -0300

    Implementando o find com populate

--- src/app/controllers/livro.js
+++ src/app/controllers/livro.js
@@ -3,24 +3,46 @@ const autor = require('./../models/autor')
 
 class Livro {
 
-    get(req, res) {
-        livro.find({}, (err, data) => {
-            if (err) {
-                res.status(500).json({ message: "Houve um erro ao processar sua requisição", error: err })
-            } else {
-                res.status(200).json({ message: "Dados recuperados com sucesso", dados: data })
-            }
-        })
+    findAll(req, res) {
+        livro.find({})
+            .populate('autor', { nome: 1 })
+            .exec((err, data) => {
+                if (err) {
+                    res.status(500).json({ message: "Houve um erro ao processar sua requisição", error: err })
+                } else {
+                    if (data.length <= 0) {
+                        res.status(200).json({ message: "Não foram encontrados livros para listar" })
+                    } else {
+                        res.status(200).json({ message: "Livros recuperados com sucesso", data: data })
+                    }
+                }
+            })
     }
 
-    getByName(req, res) {
-        livro.find({ nome: req.params.nome }, (err, data) => {
-            if (err) {
-                res.status(500).json({ message: "Houve um erro ao processar sua requisição", error: err })
-            } else {
-                res.status(200).json({ message: "Dado recuperado com sucesso", dado: data })
-            }
-        })
+    findById(req, res) {
+        const { idBook } = req.params
+
+        livro.findById(idBook)
+            .populate('autor', { nome: 1 })
+            .exec((err, data) => {
+                if (err) {
+                    res.status(500).json({ message: "Houve um erro ao processar sua requisição", error: err })
+                } else {
+                    if (data.length <= 0) {
+                        res.status(200).json({ message: "O livro solicitado não foi encontrado" })
+                    } else {
+                        res.status(200).json({ message: "Livro recuperado com sucesso", data: data })
+                    }
+                }
+            })
+
+        // livro.find({ nome: req.params.nome }, (err, data) => {
+        //     if (err) {
+        //         res.status(500).json({ message: "Houve um erro ao processar sua requisição", error: err })
+        //     } else {
+        //         res.status(200).json({ message: "Dado recuperado com sucesso", dado: data })
+        //     }
+        // })
     }
 
     create(req, res) {
