8f45dc8d1ed1c86a862c14f645e596520567583f
hppod <hppodgornik@gmail.com>
2020-08-24 13:52:06 -0300

    Removendo put e delete de livro

--- src/app/routes/livro.js
+++ src/app/routes/livro.js
@@ -5,7 +5,5 @@ const Livro = require("./../controllers/livro")
 route.get("/listarTodos", Livro.findAll)
 route.get("/listarUm/:bookName", Livro.findById)
 route.post("/criar", Livro.create)
-// route.put("/atualizar/:nome", Livro.put)
-// route.delete("/apagar/:nome", Livro.delete)
 
 module.exports = route
